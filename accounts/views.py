d56b0c548fa20248404ac537adfaf2291242d52d
shahbajkhan2023 <rk2403476@gmail.com>
2024-09-13 16:37:21 +0530

    cart_functionality

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,8 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
 from vendor.models import Vendor
+from django.template.defaultfilters import slugify
+
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -76,7 +78,7 @@ def registerUser(request):
 def registerVendor(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
-        return redirect('vendorDashboard')
+        return redirect('myAccount')
     
     elif request.method == 'POST':
         # store the data and create the user
@@ -92,6 +94,8 @@ def registerVendor(request):
             user.role = User.VENDOR
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'accounts',
     'vendor',
     'menu',
+    'marketplace',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'marketplace.context_processors.get_cart_counter',
+                'marketplace.context_processors.get_cart_amounts',
             ],
         },
     },
