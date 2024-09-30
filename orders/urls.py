11ad911fd660d6f1797a24fa3403754ec4d21b2c
shahbajkhan2023 <rk2403476@gmail.com>
2024-09-30 17:48:57 +0530

    payment view

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('place-order/', views.PlaceOrderView.as_view(), name='place_order'),
+    path('payments/', views.PaymentsView.as_view(), name='payments'),
 ]
